1482c2b448dfb6d958aaef2572465c65130f6e25
Chiara Nuñez <chiaraani@gmail.com>
2024-02-07 17:29:10 +0000

    Add link to open example in a new tab

--- docs/assets/snippet.js
+++ docs/assets/snippet.js
@@ -2,15 +2,23 @@ addSnippets()
 
 function addSnippets() {
   for (let codeExample of document.getElementsByClassName('language-html')) {
-    const example = document.createElement('iframe')
-
-    // Building iframe source URL
-    const url = new URL('../example.html', document.baseURI)
-    url.searchParams.append('code', codeExample.textContent)
-    example.src = url.href
-
+    const example = document.createElement('div')
     example.classList.add('example')
 
+    // Building source URL
+    const url = new URL('../example.html', document.baseURI)
+    url.searchParams.append('code', codeExample.textContent)
+    
+    const link = document.createElement('a')
+    link.textContent = 'Open example in a new tab ⇱'
+    link.target = "_blank"
+    link.href = url
+    example.append(link)
+
+    const iframe = document.createElement('iframe')
+    iframe.src = url.href
+    example.append(iframe)
+    
     codeExample.after(example)
   }
   iframeResizing()
